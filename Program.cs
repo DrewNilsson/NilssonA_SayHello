b7c4146bd386c7ef8bf68165486e30d89bb5772f
DrewNilsson <nilsson1322@gmail.com>
2022-10-18 11:55:27 -0700

    First push

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Andrew Nilsson
+//09-07-2022
+//Mini Challenge #1 - Say Hello
+//This is a console project that will say hello
+//Peer Review by: Caroline - great amazing ran good and was funny !
+
+string play = "yes";
+while (play == "yes")
+{
+
+    Console.WriteLine("What is your Name?");
+
+    string? myName = Console.ReadLine();
+
+    Console.WriteLine("Say hello " + myName);
+
+    string? playInput = "";
+    while (playInput != "yes" && playInput != "no")
+    {
+        Console.WriteLine("\n" + "If you would like to enter another name type yes, enter no if not.");
+        playInput = Console.ReadLine();
+
+        playInput = playInput.ToLower();
+
+        if (playInput == "yes")
+        {
+            play = playInput;
+        }
+        else if (playInput == "no")
+        {
+            Console.WriteLine($"Have a good day {myName}");
+            play = playInput;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+
+    }
+
+}
